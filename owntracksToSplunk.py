d6854a64e27cd7ccfdda365f1b438ca10d3bed00
Jeffrey Stone <thejeffreystone@gmail.com>
2016-10-31 14:51:33 -0400

    Cleaned up spacing because standards

--- owntracksToSplunk.py
+++ owntracksToSplunk.py
@@ -11,21 +11,26 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 message = 'ON'
+
+
 def on_connect(mosq, obj, rc):
     mqttc.subscribe("owntracks/#", 0)
     print("rc: " + str(rc))
 
+
 def on_message(mosq, obj, msg):
     global message
-    topic=OwnTracks2Data(msg.topic)
-    data=ast.literal_eval(msg.payload)
+    topic = OwnTracks2Data(msg.topic)
+    data = ast.literal_eval(msg.payload)
     data.update(topic)
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(data)
     getTheHECInThere(data)
 
+
 def on_subscribe(mosq, obj, mid, granted_qos):
     print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
 
 def OwnTracks2Data(topic):
     # this function was taken from mqttwarn (https://github.com/jpmens/mqttwarn)
@@ -41,17 +46,19 @@ def OwnTracks2Data(topic):
         return dict(username=username, device=deviceid)
     return None
 
+
 def getTheHECInThere(data):
     http_event_collector_key = os.environ.get("HEC_KEY")
     http_event_collector_host = os.environ.get("HEC_HOST")
     event = http_event_collector(http_event_collector_key, http_event_collector_host)
     payload = {}
-    payload.update({"index":"owntracks"})
-    payload.update({"sourcetype":"owntracks"})
-    payload.update({"source":"mqtt-owntracks"})
-    payload.update({"host":"tightbeam"})
-    payload.update({"event":data})
+    payload.update({"index": "owntracks"})
+    payload.update({"sourcetype": "owntracks"})
+    payload.update({"source": "mqtt-owntracks"})
+    payload.update({"host": "tightbeam"})
+    payload.update({"event": data})
     event.sendEvent(payload)
+
 
 mqttc = mqtt.Client()
 # Assign event callbacks
